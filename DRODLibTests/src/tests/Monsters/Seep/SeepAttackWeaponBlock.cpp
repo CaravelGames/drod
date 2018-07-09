635558c2980de88e96556cdce0fe09bea1eb5c26
Hypexion <clockoutguy@yahoo.co.uk>
2018-07-09 18:15:25 +0100

    Correct test starting positions.

--- DRODLibTests/src/tests/Monsters/Seep/SeepAttackWeaponBlock.cpp
+++ DRODLibTests/src/tests/Monsters/Seep/SeepAttackWeaponBlock.cpp
@@ -7,40 +7,44 @@
 TEST_CASE("Seep interaction with weapons on targets", "[game][player][seep]") {
 	SECTION("Staff should block Seep attack") {
 		RoomBuilder::Plot(T_WALL, 10, 10);
+		RoomBuilder::AddMonster(M_SEEP, 10, 10);
 		RoomBuilder::AddMonsterWithWeapon(M_MIMIC, WT_Staff, 8, 10, E);
 
 		CCueEvents CueEvents;
-		Runner::StartGame(19, 10, N);
+		Runner::StartGame(9, 10, N);
 		Runner::ExecuteCommand(CMD_WAIT, CueEvents);
 		REQUIRE(!CueEvents.HasOccurred(CID_MonsterKilledPlayer));
 	}
 
 	SECTION("Spear should block Seep attack") {
 		RoomBuilder::Plot(T_WALL, 10, 10);
+		RoomBuilder::AddMonster(M_SEEP, 10, 10);
 		RoomBuilder::AddMonsterWithWeapon(M_MIMIC, WT_Spear, 8, 10, E);
 
 		CCueEvents CueEvents;
-		Runner::StartGame(19, 10, N);
+		Runner::StartGame(9, 10, N);
 		Runner::ExecuteCommand(CMD_WAIT, CueEvents);
 		REQUIRE(!CueEvents.HasOccurred(CID_MonsterKilledPlayer));
 	}
 
 	SECTION("Pickaxe should block Seep attack") {
 		RoomBuilder::Plot(T_WALL, 10, 10);
+		RoomBuilder::AddMonster(M_SEEP, 10, 10);
 		RoomBuilder::AddMonsterWithWeapon(M_MIMIC, WT_Pickaxe, 8, 10, E);
 
 		CCueEvents CueEvents;
-		Runner::StartGame(19, 10, N);
+		Runner::StartGame(9, 10, N);
 		Runner::ExecuteCommand(CMD_WAIT, CueEvents);
 		REQUIRE(!CueEvents.HasOccurred(CID_MonsterKilledPlayer));
 	}
 
 	SECTION("Dagger should not block Seep attack") {
 		RoomBuilder::Plot(T_WALL, 10, 10);
+		RoomBuilder::AddMonster(M_SEEP, 10, 10);
 		RoomBuilder::AddMonsterWithWeapon(M_MIMIC, WT_Dagger, 8, 10, E);
 
 		CCueEvents CueEvents;
-		Runner::StartGame(19, 10, N);
+		Runner::StartGame(9, 10, N);
 		Runner::ExecuteCommand(CMD_WAIT, CueEvents);
 		REQUIRE(CueEvents.HasOccurred(CID_MonsterKilledPlayer));
 	}
