56a9b6ef20483db78740fbab935d5bab0174c243
Hypexion <clockoutguy@yahoo.co.uk>
2019-04-12 16:43:42 +0100

    Briar Immune Behaviour part one

--- DRODLib/Character.cpp
+++ DRODLib/Character.cpp
@@ -719,6 +719,7 @@ bool CCharacter::OnStabbed(CCueEvents &CueEvents, const UINT /*wX*/, const UINT 
 		return false;
 
 	CueEvents.Add(CID_MonsterDiedFromStab, this);
+	RefreshBriars();
 	return true;
 }
 
@@ -5136,6 +5137,8 @@ void CCharacter::Disappear()
 
 	this->bVisible = false;
 	this->bWeaponSheathed = true;
+	RefreshBriars();
+
 	ASSERT(room.pMonsterSquares[room.ARRAYINDEX(this->wX,this->wY)] == this);
 	room.pMonsterSquares[room.ARRAYINDEX(this->wX,this->wY)] = NULL;
 }
@@ -5196,6 +5199,7 @@ void CCharacter::PushInDirection(int dx, int dy, bool bStun, CCueEvents &CueEven
 
 	CMonster::PushInDirection(dx, dy, bStun, CueEvents);
 	SetWeaponSheathed();
+	RefreshBriars();
 
 	if (HasSword())
 	{
@@ -5253,6 +5257,7 @@ void CCharacter::MoveCharacter(
 	}
 
 	SetWeaponSheathed();
+	RefreshBriars();
 
 	//If player was stepped on, kill him.
 	if (!this->bSafeToPlayer && this->pCurrentGame->IsPlayerAt(this->wX, this->wY))
@@ -5260,6 +5265,17 @@ void CCharacter::MoveCharacter(
 		CCurrentGame *pGame = (CCurrentGame*)this->pCurrentGame; //non-const
 		pGame->SetDyingEntity(&pGame->swordsman, this);
 		CueEvents.Add(CID_MonsterKilledPlayer, this);
+	}
+}
+
+//*****************************************************************************
+void CCharacter::RefreshBriars()
+// Refresh briars if the NPC can block them
+// Do so by acting as if a new tile has been plotted at the character's position
+{
+	if (behaviorFlags.count(ScriptFlag::BriarImmune)) {
+		CDbRoom& room = *(this->pCurrentGame->pRoom);
+		room.briars.plotted(this->wX, this->wY, T_EMPTY);
 	}
 }
 
