CaravelGames/drod

commit f479ff4863f524216631dc452b7ac533d8a70422

Reworked death animation to no longer cache most of the room in a surface and doing shenanigans with effects and other things which caused a lot of issues with image overlays on lower levels, some effects not playing etc. This also makes the death animations much smoother

DROD/GameScreen.cpp
@@ -2836,6 +2836,8 @@ int CGameScreen::HandleEventsForPlayerDeath(CCueEvents &CueEvents)
 
 	// And we want image events to be rendered too
 	ProcessImageEvents(CueEvents, this->pRoomWidget, this->pCurrentGame);
+
+	ProcessFuseBurningEvents(CueEvents);
 	
 	//Need player falling to draw last (on top of) other effects migrated below to the m-layer
 	if (bPlayerFellInPit && bPlayerDied) {
@@ -2862,23 +2864,10 @@ int CGameScreen::HandleEventsForPlayerDeath(CCueEvents &CueEvents)
 
 	//Show the screen after first arriving here.
 	this->pFaceWidget->SetReading(false);
-	this->pRoomWidget->RemoveTLayerEffectsOfType(ESPARK);	//stop showing where bombs were
 	this->pRoomWidget->RemoveMLayerEffectsOfType(EPENDINGBUILD); //stop showing where pending building was
-	this->pRoomWidget->RemoveLastLayerEffectsOfType(EEVILEYEGAZE);
 	this->pRoomWidget->RemoveHighlight();
-	this->pRoomWidget->RemoveLastLayerEffectsOfType(ESHADE); //remove user tile highlight
-	this->pRoomWidget->PutTLayerEffectsOnMLayer();	//keep showing whatever effects were showing
 	this->pRoomWidget->AllowSleep(false);
 	this->pRoomWidget->Paint();
-
-	//Prepare room for fade out.
-	this->pRoomWidget->RenderRoomInPlay();
-	this->pRoomWidget->DrawOverheadLayer(this->pRoomWidget->pRoomSnapshotSurface);
-	this->pRoomWidget->DrawGhostOverheadCharacters(this->pRoomWidget->pRoomSnapshotSurface, false);
-	this->pRoomWidget->RenderEnvironment(this->pRoomWidget->pRoomSnapshotSurface);
-
-	const bool bFade = g_pTheBM->bAlpha;
-	CFade *pFade = bFade ? new CFade(this->pRoomWidget->pRoomSnapshotSurface,NULL) : NULL;
 
 	bool bNonMonsterDeath = false;
 	const CMonster *pHalph = NULL;
@@ -3029,17 +3018,15 @@ int CGameScreen::HandleEventsForPlayerDeath(CCueEvents &CueEvents)
 		}
 
 		//Fade to black.
-		if (dwNow - dwLastFade > 100 && g_pTheBM->bAlpha)
+		if (g_pTheBM->bAlpha && dwStart)
 		{
-			if (bFade)
-			{
-				const float fFade = (dwNow - dwStart) / (float)dwDeathDuration;
-				pFade->IncrementFade(fFade);
-				this->pRoomWidget->SetOpacityForMLayerEffectsOfType(ESNOWFLAKE, 1.0f-fFade);
-				this->pRoomWidget->SetOpacityForMLayerEffectsOfType(ERAINDROP, 1.0f-fFade);
-				this->pRoomWidget->SetOpacityForEffectsOfType(EIMAGEOVERLAY, 1.0f-fFade);
-			}
+			const float remainingFraction = 1 - (dwNow - dwStart) / (float)dwDeathDuration;
+
+			this->pRoomWidget->SetDeathFadeOpacity(remainingFraction);
+			this->pRoomWidget->pMLayerEffects->SetOpacityForEffects(remainingFraction);
+			this->pRoomWidget->pLastLayerEffects->SetOpacityForEffects(remainingFraction);
 			this->pRoomWidget->DirtyRoom();  //repaint whole room each fade
+			
 			dwLastFade = dwNow;
 		}
 

DROD/RoomWidget.cpp
@@ -367,6 +367,7 @@ CRoomWidget::CRoomWidget(
 
 	, CX_TILE(CBitmapManager::CX_TILE), CY_TILE(CBitmapManager::CY_TILE)
 
+	, fDeathFadeOpacity(0)
 	, time_of_last_weather_render(0)
 	, redrawingRowForWeather(0)
 	, need_to_update_room_weather(false)
@@ -2753,10 +2754,6 @@ void CRoomWidget::RenderRoomInPlay(
 	if (bStateChanged)
 		this->bAllDirty = true;
 
-	ASSERT(this->pRoomSnapshotSurface);
-	SDL_Surface *pDestSurface = this->pRoomSnapshotSurface;  //draw to here
-	ASSERT(!SDL_MUSTLOCK(pDestSurface));   //Don't need to lock this surface.
-
 	//1. Apply current light calculations.
 	if (this->bRenderPlayerLight) //render player light when flagged
 	{
@@ -2771,23 +2768,6 @@ void CRoomWidget::RenderRoomInPlay(
 
 	//2. Draw o- and t-layers.
 	RenderRoom(wCol, wRow, wWidth, wHeight, false);
-
-	//3. When action is frozen, draw the following here.
-	if (bPlayerIsDying)
-	{
-		//a. Effects that go on top of room image, under monsters/swordsman.
-		RenderFogInPit(pDestSurface);
-
-		DrawTLayer(pDestSurface);
-		
-		this->pTLayerEffects->DrawEffects( bPlayerIsDying ? NULL : pDestSurface);
-
-		//b. Draw monsters (not killing player).
-		DrawMonsters(this->pRoom->pFirstMonster, pDestSurface, bPlayerIsDying);
-
-		//c. Effects that go on top of monsters/swordsman.
-		this->pMLayerEffects->DrawEffects(bPlayerIsDying ? NULL : pDestSurface);
-	}
 
 	this->bWasPlacingDouble = bIsPlacingDouble;
 	this->bWasInvisible = bIsInvisible;
@@ -4431,34 +4411,29 @@ void CRoomWidget::Paint(
 	//3. Draw room sprites.
 	const bool monstersAreAnimated = bMoveAnimationInProgress || bWasApplyingJitter;
 	{
-		//When action is not frozen, draw the following:
-		if (bPlayerIsAlive)
+		//3a. Draw effects that go on top of room image, under monsters/swordsman.
+		RenderFogInPit(pDestSurface);
+
+		DrawTLayer(pDestSurface, false, bMoveAnimationInProgress);
+
+		this->pTLayerEffects->DrawEffects();
+		this->pTLayerEffects->DirtyTiles();
+
+		//3b. Repaint monsters.
+		if (this->bAllDirty || monstersAreAnimated)
 		{
-			//3a. Draw effects that go on top of room image, under monsters/swordsman.
-			RenderFogInPit(pDestSurface);
-
-			DrawTLayer(pDestSurface, false, bMoveAnimationInProgress);
-
-			this->pTLayerEffects->DrawEffects();
-			this->pTLayerEffects->DirtyTiles();
-
-			//3b. Repaint monsters.
-			if (this->bAllDirty || monstersAreAnimated)
-			{
-				//Draw monsters (that aren't killing swordsman).
-				DrawMonsters(room.pFirstMonster, pDestSurface, false,
-					monstersAreAnimated);
-			} else {
-				RedrawMonsters(pDestSurface);
-			}
-
-			PopulateBuildMarkerEffects(room);
+			//Draw monsters (that aren't killing swordsman).
+			DrawMonsters(room.pFirstMonster, pDestSurface, false,
+				monstersAreAnimated);
 		} else {
-			//Add certain images as static during death animation
-
-			this->pOLayerEffects->DrawEffects(NULL, EIMAGEOVERLAY);
-			this->pOLayerEffects->DirtyTiles();
+			RedrawMonsters(pDestSurface);
 		}
+
+		if (bPlayerIsAlive)
+			PopulateBuildMarkerEffects(room);
+
+		if (this->fDeathFadeOpacity > 0)
+			g_pTheBM->DarkenRect(this->x, this->y, this->w, this->h, this->fDeathFadeOpacity, pDestSurface);
 
 		//4. Draw player.
 		if (this->bShowingPlayer && !bIsPlacingDouble)
@@ -4510,7 +4485,7 @@ void CRoomWidget::Paint(
 				DrawInvisibilityRange(this->pCurrentGame->swordsman.wDoubleCursorX,
 					this->pCurrentGame->swordsman.wDoubleCursorY, pDestSurface);
 			}
-		} else if (bPlayerIsAlive) {
+		} else {
 			//6. Draw overhead layer.
 			DrawOverheadLayer(pDestSurface);
 
@@ -7342,7 +7317,11 @@ void CRoomWidget::DrawOverheadLayer(SDL_Surface *pDestSurface)
 						nI += wHeight;
 					src.y = nI % wHeight;
 
+					if (this->fDeathFadeOpacity > 0)
+						EnableSurfaceBlending(pTileTexture, 255 * this->fDeathFadeOpacity);
 					SDL_BlitSurface(pTileTexture, &src, pDestSurface, &dest);
+					if (this->fDeathFadeOpacity > 0)
+						DisableSurfaceBlending(pTileTexture);
 				}
 			}
 		}

DROD/RoomWidget.h
@@ -339,6 +339,7 @@ public:
 	void           ResetJitter();
 	void           ResetRoom() {this->pRoom = NULL;}
 	void           SetAnimateMoves(const bool bAnimate) {this->bAnimateMoves = bAnimate;}
+	void           SetDeathFadeOpacity(const float opacity) { this->fDeathFadeOpacity = opacity;  }
 	void           SetEffectsFrozen(const bool bIsFrozen);
 	void           SetMoveDuration(const UINT dwDuration) {this->dwMoveDuration = dwDuration;}
 	void           SetOpacityForMLayerEffectsOfType(const EffectType eEffectType, float fOpacity);
@@ -643,6 +644,7 @@ private:
 	void           flag_weather_refresh();
 	void           SetFrameVarsForWeather();
 
+	float          fDeathFadeOpacity;
 	Uint32         time_of_last_weather_render;
 	int            redrawingRowForWeather;
 	bool           need_to_update_room_weather;

FrontEndLib/EffectList.cpp
@@ -362,6 +362,16 @@ SDL_Rect CEffectList::GetBoundingBoxForEffectsOfType(const UINT eEffectType) con
 }
 
 //*****************************************************************************
+void CEffectList::SetOpacityForEffects(const float fOpacity) const
+{
+	for (list<CEffect*>::const_iterator iSeek = this->Effects.begin();
+		iSeek != this->Effects.end(); ++iSeek)
+	{
+		(*iSeek)->SetOpacity(fOpacity);
+	}
+}
+
+//*****************************************************************************
 void CEffectList::SetOpacityForEffectsOfType(const UINT eEffectType, float fOpacity) const
 {
 	for (list<CEffect *>::const_iterator iSeek = this->Effects.begin();

FrontEndLib/EffectList.h
@@ -55,7 +55,8 @@ public:
 	CEffect*       GetEffectOfType(const UINT eEffectType) const;
 	SDL_Rect       GetBoundingBoxForEffectsOfType(const UINT eEffectType) const;
 	virtual void   RemoveEffectsOfType(const UINT eEffectType);
-	void           SetOpacityForEffectsOfType(const UINT eEffectType, float fOpacity) const;
+	void           SetOpacityForEffects(const float fOpacity) const;
+	void           SetOpacityForEffectsOfType(const UINT eEffectType, const float fOpacity) const;
 	void           SetEffectsFrozen(const bool bIsFrozen);
 
 	list<CEffect *> Effects;

commit 7ad9c8449792c794f2431b7879ad13f65875aea9

Fixed an issue introduced in f479ff4863f524216631dc452b7ac533d8a70422 that would cause the room reappear into an unfaded state for fraction of a second at the end of death animation

DROD/GameScreen.cpp
@@ -3020,9 +3020,10 @@ int CGameScreen::HandleEventsForPlayerDeath(CCueEvents &CueEvents)
 		//Fade to black.
 		if (g_pTheBM->bAlpha && dwStart)
 		{
-			const float remainingFraction = 1 - (dwNow - dwStart) / (float)dwDeathDuration;
+			const float durationFraction = min(1, (dwNow - dwStart) / (float)dwDeathDuration);
+			const float remainingFraction = 1 - durationFraction;
 
-			this->pRoomWidget->SetDeathFadeOpacity(remainingFraction);
+			this->pRoomWidget->SetDeathFadeOpacity(durationFraction);
 			this->pRoomWidget->pMLayerEffects->SetOpacityForEffects(remainingFraction);
 			this->pRoomWidget->pLastLayerEffects->SetOpacityForEffects(remainingFraction);
 			this->pRoomWidget->DirtyRoom();  //repaint whole room each fade
@@ -3078,7 +3079,12 @@ int CGameScreen::HandleEventsForPlayerDeath(CCueEvents &CueEvents)
 		if (dwNow - dwStart > dwDeathDuration)
 			break;
 	}
-	delete pFade;
+	
+	this->pRoomWidget->preventDrawing = true;
+
+	this->pRoomWidget->SetDeathFadeOpacity(0);
+	this->pRoomWidget->pMLayerEffects->SetOpacityForEffects(1);
+	this->pRoomWidget->pLastLayerEffects->SetOpacityForEffects(1);
 	this->pCurrentGame->swordsman.SetOrientation(wOrigO);	//restore value
 
 	if (bPlayerFellInPit)
@@ -4516,9 +4522,6 @@ SCREENTYPE CGameScreen::ProcessCueEventsBeforeRoomDraw(
 		}
 		CueEvents.Clear();	//clear after death sequence (whether move is undone or not)
 		//but before room restart so speech on room start can be retained
-
-		// Image overlay opacity must be restored, otherwise they'll keep their opacity while playing until they are regenerated
-		this->pRoomWidget->SetOpacityForEffectsOfType(EIMAGEOVERLAY, 1.0f);
 
 		ASSERT(!this->pCurrentGame->bIsGameActive || bUndoDeath);
 		if (GetScreenType() == SCR_Demo)

DROD/RoomWidget.cpp
@@ -4433,7 +4433,7 @@ void CRoomWidget::Paint(
 			PopulateBuildMarkerEffects(room);
 
 		if (this->fDeathFadeOpacity > 0)
-			g_pTheBM->DarkenRect(this->x, this->y, this->w, this->h, this->fDeathFadeOpacity, pDestSurface);
+			g_pTheBM->DarkenRect(this->x, this->y, this->w, this->h, 1.0f - this->fDeathFadeOpacity, pDestSurface);
 
 		//4. Draw player.
 		if (this->bShowingPlayer && !bIsPlacingDouble)
@@ -7318,7 +7318,7 @@ void CRoomWidget::DrawOverheadLayer(SDL_Surface *pDestSurface)
 					src.y = nI % wHeight;
 
 					if (this->fDeathFadeOpacity > 0)
-						EnableSurfaceBlending(pTileTexture, 255 * this->fDeathFadeOpacity);
+						EnableSurfaceBlending(pTileTexture, 255 * (1 - this->fDeathFadeOpacity));
 					SDL_BlitSurface(pTileTexture, &src, pDestSurface, &dest);
 					if (this->fDeathFadeOpacity > 0)
 						DisableSurfaceBlending(pTileTexture);