8e954aff9f59f53c1b24d39fbb9a618288499f7c
Maurycy Zarzycki <maurycy@evidentlycube.com>
2020-11-14 11:59:47 +0100

    Fixed regression: Stalwarts will once again pathmap through tiles that would put their sword on a fegund or fegundo ash

--- DRODLib/Stalwart.cpp
+++ DRODLib/Stalwart.cpp
@@ -200,8 +200,13 @@ bool CStalwart::IsSafeToStab(const UINT wFromX, const UINT wFromY, const UINT wS
 
 	//Don't stab a friendly monster.
 	CMonster *pMonster = room.GetMonsterAtSquare(wSX, wSY);
-	if (pMonster && pMonster->IsAlive() && pMonster->IsFriendly() &&
-			pMonster != static_cast<const CMonster*>(this)) //impossible to stab self -- ignore this case
+	
+	if (pMonster
+			&& pMonster->IsAlive()
+			&& pMonster->IsFriendly()
+			&& pMonster->wType != M_FEGUNDO //Fegundos are invulnerable to sword stabs so stalwarts are free to stab them
+			&& pMonster->wType != M_FEGUNDOASHES //Fegundos are vulnerable but historically Stalwarts do not avoid them
+			&& pMonster != static_cast<const CMonster*>(this)) //impossible to stab self -- ignore this case
 		return false;
 
 	return true;
