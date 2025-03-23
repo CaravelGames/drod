fe7c6d2d5557f15dff75cecdddb976b7253b537a
Mike Rimer <mrimer@caravelgames.com>
2025-03-23 01:32:08 -0600

    Add wszTimes.

--- BackEndLib/Wchar.h
+++ BackEndLib/Wchar.h
@@ -80,7 +80,7 @@ typedef std::basic_string<WCHAR, std::char_traits<WCHAR>, std::allocator<WCHAR> 
 //Some common small strings.  Larger strings are localizable and should be kept in database.
 extern const WCHAR wszAmpersand[], wszAsterisk[], wszOpenAngle[], wszCloseAngle[], wszColon[],
 	wszComma[], wszCommaSpace[], wszCRLF[], wszDollarSign[], wszElipsis[], wszEmpty[], wszEqual[],
-	wszExclamation[], wszForwardSlash[], wszHyphen[], wszParentDir[],
+	wszExclamation[], wszForwardSlash[], wszTimes[], wszHyphen[], wszParentDir[],
 	wszPercent[], wszPeriod[], wszPoundSign[], wszPlus[], wszQuestionMark[], wszQuote[],
 	wszLeftBracket[], wszRightBracket[], wszLeftParen[], wszRightParen[],
 	wszSemicolon[], wszSpace[], wszSlash[], wszUnderscore[], wszZero[], wszTilde[],
