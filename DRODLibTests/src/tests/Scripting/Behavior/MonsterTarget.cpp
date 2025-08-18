89b5f43f2a7d8639a503f82d68e6f7399b7466e9
Hypexion <hypexionprime@gmail.com>
2025-08-18 20:44:34 +0100

    Update test case in MonsterTarget test file

--- DRODLibTests/src/tests/Scripting/Behavior/MonsterTarget.cpp
+++ DRODLibTests/src/tests/Scripting/Behavior/MonsterTarget.cpp
@@ -32,19 +32,19 @@ TEST_CASE("Scripting: Monster Targeting Behaviors", "[game][scripting][behavior]
 		AssertMonsterType(10, 10, M_ROACH);
 	}
 
-	SECTION("Test Eye will see character target") {
+	SECTION("Test Eye will see non-target character with eye-waking behavior") {
 		// For simplicity, we will make this character move before monsters
 		pCharacter->wProcessSequence = 100;
-		RoomBuilder::AddCommand(pCharacter, CCharacterCommand::CC_Behavior, ScriptFlag::MonsterTarget, 1);
+		RoomBuilder::AddCommand(pCharacter, CCharacterCommand::CC_Behavior, ScriptFlag::WakesEyes, 1);
 		RoomBuilder::AddCommand(pCharacter, CCharacterCommand::CC_MoveRel, 0, 1);
 		RoomBuilder::AddMonster(M_EYE, 5, 11, E);
 
-		CCurrentGame* pGame = Runner::StartGame(1, 1);
+		CCurrentGame* pGame = Runner::StartGame(1, 11);
 		Runner::ExecuteCommand(CMD_WAIT);
 
 		// Eye should wake up and make move
 		CHECK(pGame->pRoom->GetMonsterOfType(M_EYE)->IsAggressive());
-		AssertMonster(6, 11);
+		AssertMonster(4, 11);
 	}
 
 	SECTION("Test monster is not attracted to conditional character target until stealth is broken") {
@@ -64,27 +64,6 @@ TEST_CASE("Scripting: Monster Targeting Behaviors", "[game][scripting][behavior]
 		Runner::ExecuteCommand(CMD_S);
 
 		AssertMonsterType(9, 10, M_ROACH);
-	}
-
-	SECTION("Test Eye does not see conditional character target until stealth is broken") {
-		RoomBuilder::AddCommand(pCharacter, CCharacterCommand::CC_Behavior, ScriptFlag::MonsterTargetWhenPlayerIsTarget, 1);
-		RoomBuilder::AddCommand(pCharacter, CCharacterCommand::CC_SetPlayerStealth, Stealth_On);
-		RoomBuilder::AddMonster(M_ROACH, 1, 3);
-		RoomBuilder::AddMonster(M_EYE, 5, 10, E);
-
-		CCurrentGame* pGame = Runner::StartGame(1, 1, S);
-		Runner::ExecuteCommand(CMD_WAIT);
-
-		// Eye should not wake up and make move
-		CHECK(!pGame->pRoom->GetMonsterOfType(M_EYE)->IsAggressive());
-		AssertMonster(5, 10);
-
-		// Kill a monster to break stealth
-		Runner::ExecuteCommand(CMD_S);
-
-		// Eye should now wake up and make move
-		CHECK(pGame->pRoom->GetMonsterOfType(M_EYE)->IsAggressive());
-		AssertMonster(6, 10);
 	}
 
 	SECTION("Test monster kills attackable non-target on way to target") {
